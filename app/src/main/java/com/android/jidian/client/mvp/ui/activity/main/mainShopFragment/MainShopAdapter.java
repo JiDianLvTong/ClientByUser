246cbf1876636f18efaf2bd64c5845efc89264df
Guo <603050562@qq.com>
2023-01-09 23:47:13 +0800

    Guo_2023_1_9_23_49_商城页卡

--- app/src/main/java/com/android/jidian/client/mvp/ui/activity/main/mainShopFragment/MainShopAdapter.java
+++ app/src/main/java/com/android/jidian/client/mvp/ui/activity/main/mainShopFragment/MainShopAdapter.java
@@ -1,104 +1,31 @@
 package com.android.jidian.client.mvp.ui.activity.main.mainShopFragment;
 
-import android.graphics.Color;
-import android.text.TextUtils;
-import android.view.View;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.TextView;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
 
-import com.android.jidian.client.R;
-import com.android.jidian.client.bean.ShopBuyBean;
-import com.android.jidian.client.bean.ShopRentBean;
-import com.bumptech.glide.Glide;
-import com.chad.library.adapter.base.BaseQuickAdapter;
-import com.chad.library.adapter.base.BaseViewHolder;
+public class MainShopAdapter extends FragmentPagerAdapter {
 
-import java.util.List;
+    private String[] mTitle;
+    private MainShopItemFragment[] fragments;
 
-/**
- * @author : xiaoming
- * date: 2022/12/29 16:24
- * company: 兴达智联
- * description:
- */
-public class MainShopAdapter extends BaseQuickAdapter<MainShopBean, BaseViewHolder> {
-
-    public MainShopAdapter() {
-        super(R.layout.u6_activity_main_fragment_shop_item);
+    public MainShopAdapter(FragmentManager fm, MainShopItemFragment[] fragments, String[] pageNames) {
+        super(fm);
+        this.fragments = fragments;
+        this.mTitle = pageNames;
     }
 
     @Override
-    protected void convert(BaseViewHolder helper, MainShopBean mainShopBean) {
-
-        String type = mainShopBean.getMainShopDataType();
-        if(type.equals("buy")){
-            ShopBuyBean.DataBean.PacksBean bean = (ShopBuyBean.DataBean.PacksBean) mainShopBean.getObjectBean();
-            String color = bean.getFcolor();
-            helper.setTextColor(R.id.tv_main_shop_item_title, Color.parseColor(color));
-            helper.setTextColor(R.id.tv_main_shop_item_price, Color.parseColor(color));
-            helper.setTextColor(R.id.tv_main_shop_item_price_unit, Color.parseColor(color));
-            helper.setText(R.id.tv_main_shop_item_title,bean.getName());
-            helper.setText(R.id.tv_main_shop_item_price,bean.getRprice());
-            Glide.with(mContext).load(bean.getBg_img()).into(((ImageView)helper.getView(R.id.iv_main_shop_bg)));
-            List<ShopBuyBean.DataBean.PacksBean.MainsBean> mainsBean = bean.getMains();
-            ((LinearLayout)helper.getView(R.id.ll_main_shop_item_detail)).removeAllViews();
-            if (mainsBean.size() > 0) {
-                for (int i = 0; i < mainsBean.size(); i++) {
-                    if (!TextUtils.isEmpty(mainsBean.get(0).getName())) {
-                        if (mainsBean.get(0).getName().contains("卡")) {
-                            break;
-                        }
-                    }
-                    LinearLayout linearLayout = (LinearLayout) View.inflate(mContext, R.layout.main_shop_recyclerview_item_item, null);
-                    if (i != 0) {
-                        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-                        layoutParams.setMargins(20, 0, 0, 0);
-                        linearLayout.setLayoutParams(layoutParams);
-                    }
-                    TextView t_1 = linearLayout.findViewById(R.id.t_1);
-                    TextView t_2 = linearLayout.findViewById(R.id.t_2);
-                    t_1.setText(mainsBean.get(i).getName());
-                    t_1.setTextColor(Color.parseColor(color));
-                    t_2.setText(mainsBean.get(i).getSize());
-                    t_2.setTextColor(Color.parseColor(color));
-                    ((LinearLayout)helper.getView(R.id.ll_main_shop_item_detail)).addView(linearLayout);
-                }
-            }
-        }else if(type.equals("rent")){
-            ShopRentBean.DataBean.PacksBean bean = (ShopRentBean.DataBean.PacksBean) mainShopBean.getObjectBean();
-            String color = bean.getFcolor();
-            helper.setTextColor(R.id.tv_main_shop_item_title, Color.parseColor(color));
-            helper.setTextColor(R.id.tv_main_shop_item_price, Color.parseColor(color));
-            helper.setTextColor(R.id.tv_main_shop_item_price_unit, Color.parseColor(color));
-            helper.setText(R.id.tv_main_shop_item_title,bean.getName());
-            helper.setText(R.id.tv_main_shop_item_price,bean.getRprice());
-            Glide.with(mContext).load(bean.getBg_img()).into(((ImageView)helper.getView(R.id.iv_main_shop_bg)));
-            List<ShopRentBean.DataBean.PacksBean.MainsBean.LeftBean> mainsBean = bean.getMains().getLeft();
-            ((LinearLayout)helper.getView(R.id.ll_main_shop_item_detail)).removeAllViews();
-            if (mainsBean.size() > 0) {
-                for (int i = 0; i < mainsBean.size(); i++) {
-                    if (!TextUtils.isEmpty(mainsBean.get(0).getName())) {
-                        if (mainsBean.get(0).getName().contains("卡")) {
-                            break;
-                        }
-                    }
-                    LinearLayout linearLayout = (LinearLayout) View.inflate(mContext, R.layout.main_shop_recyclerview_item_item, null);
-                    if (i != 0) {
-                        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-                        layoutParams.setMargins(20, 0, 0, 0);
-                        linearLayout.setLayoutParams(layoutParams);
-                    }
-                    TextView t_1 = linearLayout.findViewById(R.id.t_1);
-                    TextView t_2 = linearLayout.findViewById(R.id.t_2);
-                    t_1.setText(mainsBean.get(i).getName());
-                    t_1.setTextColor(Color.parseColor(color));
-                    t_2.setText(mainsBean.get(i).getSize());
-                    t_2.setTextColor(Color.parseColor(color));
-                    ((LinearLayout)helper.getView(R.id.ll_main_shop_item_detail)).addView(linearLayout);
-                }
-            }
-        }
+    public Fragment getItem(int i) {
+        return fragments[i];
     }
 
+    @Override
+    public int getCount() {
+        return fragments.length;
+    }
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mTitle[position];
+    }
 }
--- app/src/main/java/com/android/jidian/client/mvp/ui/activity/main/mainShopFragment/MainShopFragment.java
+++ app/src/main/java/com/android/jidian/client/mvp/ui/activity/main/mainShopFragment/MainShopFragment.java
@@ -1,56 +1,42 @@
 package com.android.jidian.client.mvp.ui.activity.main.mainShopFragment;
 
 
-import android.content.Intent;
-import android.graphics.Color;
-
-import androidx.annotation.NonNull;
-import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager.widget.ViewPager;
 
-import android.util.Log;
 import android.view.View;
 import android.widget.LinearLayout;
 
 import com.android.jidian.client.R;
 import com.android.jidian.client.base.BaseFragment;
+import com.android.jidian.client.base.U6BaseFragment;
 import com.android.jidian.client.bean.ShopBuyBean;
 import com.android.jidian.client.bean.ShopRentBean;
 import com.android.jidian.client.bean.UserPersonalBean;
 import com.android.jidian.client.mvp.contract.MainShopContract;
 import com.android.jidian.client.mvp.presenter.MainShopPresenter;
-import com.android.jidian.client.mvp.ui.activity.pay.PayByCreateOrderActivity;
-import com.android.jidian.client.mvp.ui.activity.pay.PayByCreateOrderZhiMaActivity;
-import com.android.jidian.client.mvp.ui.activity.userInfo.PersonalInfoAuthentication;
-import com.android.jidian.client.mvp.ui.dialog.DialogByChoiceType2;
-import com.android.jidian.client.util.UserInfoHelper;
-import com.android.jidian.client.widgets.MyToast;
-import com.chad.library.adapter.base.BaseQuickAdapter;
-import com.scwang.smart.refresh.header.MaterialHeader;
+import com.flyco.tablayout.SlidingTabLayout;
 import com.scwang.smart.refresh.layout.SmartRefreshLayout;
 
 import java.util.ArrayList;
 
 import butterknife.BindView;
 
-public class MainShopFragment extends BaseFragment<MainShopPresenter> implements MainShopContract.View {
+public class MainShopFragment extends U6BaseFragment {
 
-    @BindView(R.id.smartRefreshLayout)
-    public SmartRefreshLayout smartRefreshLayout;
-    @BindView(R.id.recyclerView)
-    public RecyclerView recyclerView;
-    @BindView(R.id.mullDataPanel)
-    public LinearLayout mullDataPanel;
+    @BindView(R.id.slidingTabLayout)
+    public SlidingTabLayout slidingTabLayout;
+    @BindView(R.id.viewPager)
+    public ViewPager viewPager;
 
-    private ArrayList<MainShopBean> arrayList = new ArrayList<>();
+    private String[] nameList = new String[]{"购买","租赁"};
+    private MainShopItemFragment[] fragments = new MainShopItemFragment[2];
     private MainShopAdapter mainShopAdapter;
 
     //获取地理位置
     private String mLng = "";
     private String mLat = "";
-
-    //获取用户信息 - 是否实名认证
-    private String idAuthTip = "";
 
     @Override
     public int getLayoutId() {
@@ -59,229 +45,28 @@ public class MainShopFragment extends BaseFragment<MainShopPresenter> implements
 
     @Override
     public void initView(View view) {
-        //mvp初始化
-        mPresenter = new MainShopPresenter();
-        mPresenter.attachView(this);
-        //下拉刷新
-        MaterialHeader materialHeader = new MaterialHeader(getActivity());
-        materialHeader.setColorSchemeColors(Color.parseColor("#D7A64A"), Color.parseColor("#D7A64A"));
-        smartRefreshLayout.setRefreshHeader(materialHeader);
-        smartRefreshLayout.setEnableHeaderTranslationContent(true);
-        smartRefreshLayout.setOnRefreshListener(new com.scwang.smart.refresh.layout.listener.OnRefreshListener() {
-            @Override
-            public void onRefresh(@NonNull com.scwang.smart.refresh.layout.api.RefreshLayout refreshLayout) {
-                if (!UserInfoHelper.getInstance().getUid().isEmpty()) {
-                    requestData();
-                } else {
-                    smartRefreshLayout.finishRefresh();
-                }
-
-            }
-        });
-        smartRefreshLayout.setEnableLoadMore(false);
-        //适配器
-        mainShopAdapter = new MainShopAdapter();
-        recyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 2));
-        mainShopAdapter.setOnItemClickListener(new BaseQuickAdapter.OnItemClickListener() {
-            @Override
-            public void onItemClick(BaseQuickAdapter baseQuickAdapter, View view, int i) {
-
-                MainShopBean mainShopBean = (MainShopBean) baseQuickAdapter.getData().get(i);
-                String type = mainShopBean.getMainShopDataType();
-                if (type.equals("buy")) {
-                    ShopBuyBean.DataBean.PacksBean bean = (ShopBuyBean.DataBean.PacksBean) mainShopBean.getObjectBean();
-                    String otype = bean.getOprice();
-                    String item_id = bean.getId();
-                    int is_buy = bean.getIs_buy();
-                    int is_sell = bean.getIs_sell();
-                    if (is_sell == -1) {
-                        showMessage("如需购买商品，请联系商家扫描商家二维码购买~");
-                    } else if (is_sell == 1) {
-                        if ("230".equals(otype) || "231".equals(otype) || "232".equals(otype)) {
-                            if (is_buy == 1) {
-                                Intent intent = new Intent(getActivity(), PayByCreateOrderActivity.class);
-                                intent.putExtra("id", item_id);
-                                intent.putExtra("from", "product");
-                                getActivity().startActivity(intent);
-                            } else {
-                                showMessage("每个用户年卡过期前，仅限购买同类型年卡~");
-                            }
-                        } else {
-                            Intent intent = new Intent(getActivity(), PayByCreateOrderActivity.class);
-                            intent.putExtra("id", item_id);
-                            intent.putExtra("from", "product");
-                            getActivity().startActivity(intent);
-                        }
-                    } else {
-                        showMessage("is_sell错误，必须是1和-1");
-                    }
-                } else if (type.equals("rent")) {
-                    ShopRentBean.DataBean.PacksBean bean = (ShopRentBean.DataBean.PacksBean) mainShopBean.getObjectBean();
-                    int is_sell = bean.getIs_sell();
-                    String id_ = bean.getId();
-                    if (is_sell == -1) {
-                        showMessage("如需租赁商品，请联系商家扫描商家二维码租赁~");
-                    } else if (is_sell == 1) {
-                        if (getActivity() != null) {
-                            if ("已认证".equals(idAuthTip)) {
-                                Intent intent = new Intent(getActivity(), PayByCreateOrderZhiMaActivity.class);
-                                intent.putExtra("id", id_);
-                                intent.putExtra("from", "product");
-                                getActivity().startActivity(intent);
-                            } else {
-                                new DialogByChoiceType2(getActivity(), "为了保障您的权益", "请先进行实名认证", new DialogByChoiceType2.DialogChoiceListener() {
-                                    @Override
-                                    public void enterReturn() {
-                                        getActivity().startActivity(new Intent(getActivity(), PersonalInfoAuthentication.class));
-                                    }
-
-                                    @Override
-                                    public void cancelReturn() {
-
-                                    }
-                                }).showPopupWindow();
-                            }
-                        }
-                    } else {
-                        MyToast.showTheToast(requireActivity(), "is_sell错误，必须是1和-1");
-                    }
-                }
-            }
-        });
-        recyclerView.setAdapter(mainShopAdapter);
-        //获取数据
-        showProgress();
-        requestData();
-    }
-
-    private void requestData() {
-        arrayList.clear();
-        mPresenter.requestShopBuy(mLng, mLat);
-        mPresenter.requestShopRent(mLng, mLat);
-        mPresenter.requestUserPersonal(UserInfoHelper.getInstance().getUid());
-    }
-
-    @Override
-    public void onHiddenChanged(boolean hidden) {
-        super.onHiddenChanged(hidden);
-        if (!hidden) {//不可见
-            if (mPresenter != null) {
-                requestData();
-            }
-        }
+        fragments[0] = new MainShopItemFragment(1,mLng , mLat);
+        fragments[1] = new MainShopItemFragment(2,mLng , mLat);
+        mainShopAdapter = new MainShopAdapter(getFragmentManager() , fragments , nameList);
+        viewPager.setAdapter(mainShopAdapter);
+        viewPager.setOffscreenPageLimit(2);
+        slidingTabLayout.setViewPager(viewPager, nameList);
     }
 
     //设置坐标
     public void setFragmentPosition(String lng, String lat) {
         this.mLng = lng;
         this.mLat = lat;
-    }
-
-    @Override
-    public void requestShopBuySuccess(ShopBuyBean bean) {
-        hideProgress();
-        smartRefreshLayout.finishRefresh();
-        if (bean.getData() != null) {
-            if (bean.getData().getPacks() != null) {
-                for (int i = 0; i < bean.getData().getPacks().size(); i++) {
-                    arrayList.add(new MainShopBean("buy", bean.getData().getPacks().get(i)));
-                }
-                ArrayList<MainShopBean> tempArrayList = new ArrayList<>();
-                for (int i = 0; i < arrayList.size(); i++) {
-                    if (arrayList.get(i).getMainShopDataType().equals("rent")) {
-                        tempArrayList.add(arrayList.get(i));
-                    }
-                }
-                for (int i = 0; i < arrayList.size(); i++) {
-                    if (arrayList.get(i).getMainShopDataType().equals("buy")) {
-                        tempArrayList.add(arrayList.get(i));
-                    }
-                }
-                arrayList = tempArrayList;
-                recyclerView.setVisibility(View.VISIBLE);
-                mullDataPanel.setVisibility(View.GONE);
-                mainShopAdapter.setNewData(arrayList);
-            } else {
-            }
+        if(fragments[0] != null){
+            fragments[0].setFragmentPosition(lng,lat);
         }
-        dataNull();
-    }
-
-    @Override
-    public void requestShopRentSuccess(ShopRentBean bean) {
-        hideProgress();
-        smartRefreshLayout.finishRefresh();
-        if (bean.getData() != null) {
-            if (bean.getData().getPacks() != null) {
-                for (int i = 0; i < bean.getData().getPacks().size(); i++) {
-                    arrayList.add(new MainShopBean("rent", bean.getData().getPacks().get(i)));
-                }
-                ArrayList<MainShopBean> tempArrayList = new ArrayList<>();
-                for (int i = 0; i < arrayList.size(); i++) {
-                    if (arrayList.get(i).getMainShopDataType().equals("rent")) {
-                        tempArrayList.add(arrayList.get(i));
-                    }
-                }
-                for (int i = 0; i < arrayList.size(); i++) {
-                    if (arrayList.get(i).getMainShopDataType().equals("buy")) {
-                        tempArrayList.add(arrayList.get(i));
-                    }
-                }
-                arrayList = tempArrayList;
-                recyclerView.setVisibility(View.VISIBLE);
-                mullDataPanel.setVisibility(View.GONE);
-                mainShopAdapter.setNewData(arrayList);
-            } else {
-            }
+        if(fragments[1] != null){
+            fragments[1].setFragmentPosition(lng,lat);
         }
-        dataNull();
-    }
-
-    @Override
-    public void requestUserPersonalSuccess(UserPersonalBean bean) {
-        hideProgress();
-        if (bean.getData() != null) {
-            idAuthTip = bean.getData().getId_auth_tip();
-        }
-    }
-
-
-    @Override
-    public void requestShopFail(String msg) {
-        hideProgress();
-        smartRefreshLayout.finishRefresh();
-        showMessage(msg);
-    }
-
-    private void dataNull() {
-        if (arrayList.size() == 0) {
-            recyclerView.setVisibility(View.GONE);
-            mullDataPanel.setVisibility(View.VISIBLE);
-        } else {
-            mullDataPanel.setVisibility(View.GONE);
-            recyclerView.setVisibility(View.VISIBLE);
-        }
-    }
-
-    @Override
-    public void showProgress() {
-        dialogByLoading.show();
-    }
-
-    @Override
-    public void hideProgress() {
-        dialogByLoading.dismiss();
-    }
-
-    @Override
-    public void onError(Throwable throwable) {
-        showMessage("无网络链接，请检查您的网络设置！");
-//        shSwipeRefreshLayout.finishRefresh();
     }
 
     @Override
     public void onDestroy() {
-        arrayList.clear();
         super.onDestroy();
     }
 
